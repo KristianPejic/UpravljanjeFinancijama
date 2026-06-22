62034235a2437d19de89573ca0ce7c9a8d7c6864
pejic <kristian.pejich@gmail.com>
2026-06-22 21:20:20 +0200

    chore: initial Spring Boot project skeleton

---
+++ src/main/java/org/example/sustavzaupravljajeosobnimfinancijama/SustavZaUpravljajeOsobnimFinancijamaApplication.java
@@ -0,0 +1,13 @@
+package org.example.sustavzaupravljajeosobnimfinancijama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SustavZaUpravljajeOsobnimFinancijamaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SustavZaUpravljajeOsobnimFinancijamaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/sustavzaupravljajeosobnimfinancijama/SustavZaUpravljajeOsobnimFinancijamaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sustavzaupravljajeosobnimfinancijama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SustavZaUpravljajeOsobnimFinancijamaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
